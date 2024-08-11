f36c50035f2d969a284e0595c5d0c584e4338ae8
KhumboMunsaka <khumbolane11@gmail.com>
2024-08-11 04:07:20 +0200

    I add a ui library

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,15 @@
+import { OrbitProgress } from "react-loading-indicators";
+
+function Spinner() {
+  return (
+    <OrbitProgress
+      variant="disc"
+      color="#37cc31"
+      size="medium"
+      text=""
+      textColor=""
+    />
+  );
+}
+
+export default Spinner;
